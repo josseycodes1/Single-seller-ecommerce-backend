32636d342a537b263bc61c7a311b7f4614b84f23
josseycodes1 <adewumijosephine1@gmail.com>
2025-09-08 10:36:32 +0100

    urls.py fix

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
+# products/urls.py
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, ProductViewSet, OrderViewSet, NewsletterSubscriptionViewSet, BannerViewSet, SellerRegisterView
 from django.urls import path, include
-from .views import CustomTokenObtainPairView,  user_profile
+from .views import CustomTokenObtainPairView, user_profile
 from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
@@ -12,23 +13,12 @@ router.register(r'newsletter', NewsletterSubscriptionViewSet)
 router.register(r'banners', BannerViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)), 
+  
     path("register/seller/", SellerRegisterView.as_view(), name="seller-register"),
     path("api/token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-     path("api/user/profile/", user_profile, name="user-profile"), 
+    path("api/user/profile/", user_profile, name="user-profile"),
+    
+  
+    path("", include(router.urls)), 
 ]
-
-# frontend can now fetch:
-# /api/products/ → All products
-# /api/categories/ → All categories
-# /api/orders/ → Orders (brand side)
-# /api/banners/ → Homepage banners
-# /api/newsletter/ → Newsletter subscribers
-
-#test in Postman
-#list all products: GET http://127.0.0.1:8000/api/products/
-#get single product: GET http://127.0.0.1:8000/api/products/1/
-#add new product: POST http://127.0.0.1:8000/api/products/ (send JSON body + image if needed)
-#update product: PUT http://127.0.0.1:8000/api/products/1/
-#delete product: DELETE http://127.0.0.1:8000/api/products/1/
