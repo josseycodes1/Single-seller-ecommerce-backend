f288bad22de28ef22df9dafea5632849f9f58ec8
josseycodes1 <adewumijosephine1@gmail.com>
2025-09-21 14:49:33 +0100

    debugging slugfields for seo

--- products/models.py
+++ products/models.py
@@ -74,7 +74,7 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     stock = models.PositiveIntegerField(default=0)
@@ -92,13 +92,22 @@ class Product(models.Model):
     class Meta:
         ordering = ['-created_at']
 
-    def __str__(self):
-        return self.name
-
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.name)
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
         
 class ProductImage(models.Model):
     product = models.ForeignKey(
